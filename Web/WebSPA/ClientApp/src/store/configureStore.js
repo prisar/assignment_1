775a8ab41407bfeec5b4823e3227354932486af4
prisar <pritimaysarkar2010@gmail.com>
2019-05-04 09:22:44 +0530

    warning

--- Web/WebSPA/ClientApp/src/store/configureStore.js
+++ Web/WebSPA/ClientApp/src/store/configureStore.js
@@ -20,7 +20,7 @@ export default function configureStore(history, initialState) {
   // In development, use the browser's Redux dev tools extension if installed
   const enhancers = [];
   const isDevelopment = process.env.NODE_ENV === 'development';
-  if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
+  if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) { // TODO : resolve depercation warning
     enhancers.push(window.devToolsExtension());
   }
 
